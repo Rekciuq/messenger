5885ef4c7eb9c5d8a1f4eaa5a6c1b4f24d8e8009
Rekciuq <danilaka76@outlook.com>
2025-11-13 10:52:45 +0200

    added tailwind

---
+++ app/pages/about.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const buttonCount = ref(0);
+</script>
+
+<template>
+    <div>
+        <span>counter's count is {{ buttonCount }}</span>
+        <button class="text-red-500" @click="buttonCount++">Click me</button>
+    </div>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,16 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-
+  css: ["./app/assets/css/main.css"],
+  vite: { plugins: [tailwindcss()] },
   modules: [
-    '@nuxt/eslint',
-    '@nuxt/fonts',
-    '@nuxt/icon',
-    '@nuxt/image',
-    '@nuxt/test-utils'
-  ]
-})
+    "@nuxt/eslint",
+    "@nuxt/fonts",
+    "@nuxt/icon",
+    "@nuxt/image",
+    "@nuxt/test-utils",
+  ],
+});
